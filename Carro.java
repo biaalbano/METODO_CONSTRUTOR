1fdafa2e85715e73a3949e4921f39d903069e296
biaalbano <biaahalbano78@gmail.com>
2023-10-24 15:06:25 +0000

    salvar

--- Carro.java
+++ Carro.java
@@ -1,29 +1,61 @@
 public class Carro{
-    private String cor;
-    private String modelo;
-    private int aro;
+    private String Cor;
+    private String Modelo;
+    private int Aro;
     private double ValorCarro;
+
+
+    public int getAro(){
+        return Aro;
+    }
+    public void setAro(int Aro){
+        this.Aro=Aro;
+    }
+
+
+    public String getMOdelo(){
+        return Modelo;
+    }
+    public void setModelo(String Modelo){
+        this.Modelo=Modelo;
+    }
+
+
+    public String getCor(){
+        return Cor;
+    }
+    public void setCor(String Cor){
+        this.Cor=Cor;
+    }
+
+
+    public double getValorCarro(){
+        return ValorCarro;
+    }
+    public void setValorCarro(double ValorCarro){
+        this.ValorCarro=ValorCarro;
+    }
 
     public Carro(){
 
     }
-    public Carro(int aro){
-        this.aro=aro;  
+    public Carro(int Aro){
+        this.Aro=Aro;  
     }
-    public Carro(int aro, String modelo){
-        this.aro=aro;
-        this.modelo=modelo
+    public Carro(int Aro, String Modelo){
+        this.Aro=Aro;
+        this.Modelo=Modelo;
     }
-    public Carro(int aro, String modelo, double ValorCarro){
-        this.aro=aro;
-        this.modelo=modelo;
+    public Carro(int Aro, String Modelo, double ValorCarro){
+        this.Aro=Aro;
+        this.Modelo=Modelo;
         this.ValorCarro=ValorCarro;
     }
-    public Carro(int aro, String modelo, double ValorCarro, String cor){
-        this.aro=aro;
-        this.modelo=modelo;
+    public Carro(int Aro, String Modelo, double ValorCarro, String Cor){
+        this.Aro=Aro;
+        this.Modelo=Modelo;
         this.ValorCarro=ValorCarro;
-        this.cor=cor;
+        this.Cor=Cor;
     }
 
     //4 atributos para o carro e faca 3 construtores
--- Principal.java
+++ Principal.java
@@ -1,5 +1,16 @@
 public class Principal{
-public static void main(String args) {
-    
+
+public static void main(String[] args) {
+
+    Carro car = new Carro();
+    Carro carAro = new Carro(14);
+    Carro carTresParametros= new Carro (14, "HB 20 2021 sense", 60000);
+    Carro carQuatroParametros = new Carro (14, "HB 2022 sense", 60000, "Branco");
+
+    System.out.println(carAro.getAro());
+    System.out.println(carTresParametros);
+    System.out.println(carAro);
+    System.out.println(carAro);
+
 }
 }
